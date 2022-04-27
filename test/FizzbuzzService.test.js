424b42a1910e735e9b7ce9c2fe977d356ffcd5f9
felix <josevelazco134@gmail.com>
2022-04-27 16:43:28 -0600

    Test applyValidationNumber function

--- test/FizzbuzzService.test.js
+++ test/FizzbuzzService.test.js
@@ -5,8 +5,8 @@ const explorers = Reader.readJsonFile("explorers.json");
 
 describe("Fizzbuzz Test suite",() =>{
     const explorersValidation = FizzbuzzService.applyValidationInExplorer(explorers);
-    test.each(explorersValidation)("1) Test applyValidationInExplorer function", (a) => {
-    
+    test.each(explorersValidation)("1.%#) Test applyValidationInExplorer function", (a) => {
+
         if(a.score % 3 ===0 && a.score % 5 === 0)
             expect(a.trick).toBe("fizzbuzz");
         else if(a.score % 3 ===0)
@@ -15,5 +15,17 @@ describe("Fizzbuzz Test suite",() =>{
             expect(a.trick).toBe("buzz");
         else
             expect(a.trick).toBe(a.score);
+    });
+
+    test("2) Test applyValidationNumber", () => {
+        const trick1 = FizzbuzzService.applyValidationNumber(1);
+        const trick2 = FizzbuzzService.applyValidationNumber(3);
+        const trick3 = FizzbuzzService.applyValidationNumber(5);
+        const trick4 = FizzbuzzService.applyValidationNumber(15);
+
+        expect(trick1).toBe(1);
+        expect(trick2).toBe("fizz");
+        expect(trick3).toBe("buzz");
+        expect(trick4).toBe("fizzbuzz");
     });
 });
