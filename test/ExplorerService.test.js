a0e342aad2f32f32624a1375e6a6d88b03af7a24
felix <josevelazco134@gmail.com>
2022-04-26 20:07:40 -0600

    Add test of getAmountOfExplorersByMission

--- test/ExplorerService.test.js
+++ test/ExplorerService.test.js
@@ -6,9 +6,13 @@ const explorers = Reader.readJsonFile("explorers.json");
 describe('ExplorerService test suite',() =>{
   test('1) Test filterByMission', () => {
     const explorersInNode = ExplorerService.filterByMission(explorers, "node");
-    expect(explorersInNode.length).toBe(10);
+    const nodeStack = explorersInNode.map((explorer) => explorer.mission);
+    nodeStack.forEach(element => {
+      expect(element).toBe("node");
+    });
   })
   test("2) Test getAmountOfExplorersByMission function", () => {
-    
+    const numberOfExplorerInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+    expect(numberOfExplorerInNode).toBe(10);
   })
 })
