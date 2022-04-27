4cd32c139b679d2ef459e621927950c6b714a1ed
felix <josevelazco134@gmail.com>
2022-04-27 14:32:43 -0600

    Create test for mission node and java

---
+++ test/ExplorerController.test.js
@@ -0,0 +1,17 @@
+// const Reader = require("./../lib/utils/Reader");
+// const ExplorerService = require("./../lib/services/ExplorerService");
+// const FizzbuzzService = require("./../lib/services/FizzbuzzService");
+const ExplorerController = require("./../lib/controllers/ExplorerController");
+
+
+describe('ExplorerController test suite',() =>{
+  const explorersInNode = ExplorerController.getExplorerByMission("node");
+  test.each(explorersInNode)("1.%#) Test getExplorerByMission in Node", (explorer) => {
+    expect(explorer.mission).toBe("node");
+  })
+
+  const explorersInJava = ExplorerController.getExplorerByMission("java");
+  test.each(explorersInJava)("2.%#) Test getExplorerByMission in Java", (explorer) => {
+    expect(explorer.mission).toBe("java");
+  })
+})
