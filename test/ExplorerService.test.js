5f414d03c29981cd100fd77bcaa046b7fc77aba9
felix <josevelazco134@gmail.com>
2022-04-26 22:10:21 -0600

    Add test for getExplorerUsernamesByMission function

--- test/ExplorerService.test.js
+++ test/ExplorerService.test.js
@@ -4,7 +4,7 @@ const ExplorerService = require('./../lib/utils/ExplorerService');
 const explorers = Reader.readJsonFile("explorers.json");
 
 describe('ExplorerService test suite',() =>{
-  test('1) Test filterByMission', () => {
+  test('1) Test filterByMission fuction', () => {
     const explorersInNode = ExplorerService.filterByMission(explorers, "node");
     const nodeStack = explorersInNode.map((explorer) => explorer.mission);
     nodeStack.forEach(element => {
@@ -14,5 +14,9 @@ describe('ExplorerService test suite',() =>{
   test("2) Test getAmountOfExplorersByMission function", () => {
     const numberOfExplorerInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
     expect(numberOfExplorerInNode).toBe(10);
+  })
+  test("3) Test getExplorersUsernamesByMission function", () => {
+    const explorersUsernameInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+    expect(explorersUsernameInNode).not.toBe("");
   })
 })
