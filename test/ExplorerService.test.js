451dfe456c146dacbc0d4e9f3ed799123baae911
felix <josevelazco134@gmail.com>
2022-04-27 18:16:12 -0600

    Add filterByStack function and change variable name

--- test/ExplorerService.test.js
+++ test/ExplorerService.test.js
@@ -6,10 +6,8 @@ const explorers = Reader.readJsonFile("explorers.json");
 describe("ExplorerService test suite",() =>{
     test("1) Test filterByMission fuction", () => {
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
-        const nodeStack = explorersInNode.map((explorer) => explorer.mission);
-        nodeStack.forEach(element => {
-            expect(element).toBe("node");
-        });
+        const isEveryMissionNode = explorersInNode.every((explorer) => explorer.mission == "node");
+        expect(isEveryMissionNode).toBe(true);
     });
     test("2) Test getAmountOfExplorersByMission function", () => {
         const numberOfExplorerInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
@@ -18,5 +16,9 @@ describe("ExplorerService test suite",() =>{
     test("3) Test getExplorersUsernamesByMission function", () => {
         const explorersUsernameInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
         expect(explorersUsernameInNode).not.toBe("");
+    });
+    const explorersWithJavascript = ExplorerService.filterByStack(explorers, "javascript");
+    test.each(explorersWithJavascript)("4.%#) Test filterByStack fuction", (explorer) => {
+        expect(explorer.stacks).toContain("javascript");
     });
 });
