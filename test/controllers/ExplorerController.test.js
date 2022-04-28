a0d1335becd240652add9a807365ecc5b5925c90
felix <josevelazco134@gmail.com>
2022-04-27 23:52:43 -0600

    Fix directories link in unit tests

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,7 +1,7 @@
 // const Reader = require("./../lib/utils/Reader");
 // const ExplorerService = require("./../lib/services/ExplorerService");
 // const FizzbuzzService = require("./../lib/services/FizzbuzzService");
-const ExplorerController = require("./../lib/controllers/ExplorerController");
+const ExplorerController = require("./../../lib/controllers/ExplorerController");
 
 
 describe("ExplorerController test suite",() =>{
--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,8 +1,9 @@
-const Reader = require("./../lib/utils/Reader");
+const Reader = require("./../../lib/utils/Reader");
 
 describe("Reader test suite",() =>{
     test("1) Test readJsonFile function", () => {
-        const explorer = Reader.readJsonFile("explorers.json");
+        // const explorer = Reader.readJsonFile("./../explorerss.json");
+        const explorer = Reader.readJsonFile("test/data/explorers.json");
         expect(typeof explorer).not.toBeUndefined();
     });
 });
